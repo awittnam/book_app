d17dcaf394f36528f9b5bfa293d5301099651d84
Adam Wittnam <adam.wittnam@hotmail.com>
2018-11-29 13:12:19 -0800

    searching

--- server.js
+++ server.js
@@ -1,80 +1,80 @@
+// 'use strict';
+
+// // Application dependencies
+// const express = require('express');
+// const cors = require('cors');
+// const pg = require('pg');
+// const superagent = require('superagent');
+
+
+// // Load environment variables from .env file
+// require('dotenv').config();
+
+// // Application Setup
+// const app = express();
+// const PORT = process.env.PORT || 4000;
+// app.set('view engine', 'ejs');// tell it we're using ejs
+// app.use(cors());
+
+// app.get('/', (request, response) => response.render('pages/index'));
+// app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
+
 'use strict';
 
-// Application dependencies
+// Application Dependencies
 const express = require('express');
-const cors = require('cors');
-const pg = require('pg');
 const superagent = require('superagent');
-
-
-// Load environment variables from .env file
-require('dotenv').config();
 
 // Application Setup
 const app = express();
 const PORT = process.env.PORT || 4000;
-app.set('view engine', 'ejs');// tell it we're using ejs
-app.use(cors());
 
-app.get('/', (request, response) => response.render('pages/index'));
-app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
+// Application Middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
 
-// 'use strict';
+// Set the view engine for server-side templating
+app.set('view engine', 'ejs');
 
-// // Application Dependencies
-// const express = require('express');
-// const superagent = require('superagent');
+// API Routes
+// Renders the search form
+app.get('/', newSearch);
 
-// // Application Setup
-// const app = express();
-// const PORT = process.env.PORT || 3000;
+// Creates a new search to the Google Books API
+app.post('/searches', createSearch);
 
-// // Application Middleware
-// app.use(express.urlencoded({extended:true}));
-// app.use(express.static('public'));
+// Catch-all
+app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
-// // Set the view engine for server-side templating
-// app.set('view engine', 'ejs');
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
-// // API Routes
-// // Renders the search form
-// app.get('/', newSearch);
+// HELPER FUNCTIONS
+// Only show part of this to get students started
+function Book(info) {
+  const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
 
-// // Creates a new search to the Google Books API
-// app.post('/searches', createSearch);
+  this.title = info.title || 'No title available';
 
-// // Catch-all
-// app.get('*', (request, response) => response.status(404).send('This route does not exist'));
+}
 
-// app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+// Note that .ejs file extension is not required
+function newSearch(request, response) {
+  response.render('pages/index');
+}
 
-// // HELPER FUNCTIONS
-// // Only show part of this to get students started
-// function Book(info) {
-//   const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+// No API key required
+// Console.log request.body and request.body.search
+function createSearch(request, response) {
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
 
-//   this.title = info.title || 'No title available';
+  console.log(request.body)
+  console.log(request.body.search)
 
-// }
+  if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
+  if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
 
-// // Note that .ejs file extension is not required
-// function newSearch(request, response) {
-//   response.render('pages/index');
-// }
-
-// // No API key required
-// // Console.log request.body and request.body.search
-// function createSearch(request, response) {
-//   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
-
-//   console.log(request.body)
-//   console.log(request.body.search)
-
-//   if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
-//   if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
-
-//   superagent.get(url)
-//     .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
-//     .then(results => response.render('pages/searches/show', {searchResults: results}))
-//     // how will we handle errors?
-// }
+  superagent.get(url)
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
+    .then(results => response.render('pages/searches/show', {searchResults: results}))
+    // how will we handle errors?
+}
