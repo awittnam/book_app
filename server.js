e2256b1b7994f677d02597698ecbcf36028546d2
Adam Wittnam <adam.wittnam@hotmail.com>
2018-11-29 13:20:51 -0800

    random cleanup

--- server.js
+++ server.js
@@ -1,33 +1,27 @@
-// 'use strict';
 
-// // Application dependencies
-// const express = require('express');
-// const cors = require('cors');
-// const pg = require('pg');
-// const superagent = require('superagent');
 
 
 // // Load environment variables from .env file
 // require('dotenv').config();
 
-// // Application Setup
-// const app = express();
-// const PORT = process.env.PORT || 4000;
-// app.set('view engine', 'ejs');// tell it we're using ejs
-// app.use(cors());
+
 
 // app.get('/', (request, response) => response.render('pages/index'));
-// app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
+
 
 'use strict';
 
 // Application Dependencies
 const express = require('express');
 const superagent = require('superagent');
+const cors = require('cors');
+const pg = require('pg');
 
 // Application Setup
 const app = express();
 const PORT = process.env.PORT || 4000;
+app.set('view engine', 'ejs');// tell it we're using ejs
+app.use(cors());
 
 // Application Middleware
 app.use(express.urlencoded({extended:true}));
